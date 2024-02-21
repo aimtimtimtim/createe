ce6d2b41ecec82d97a68222a8fb5efe9e61e2db7
Tim <agayevdev@gmail.com>
2024-02-21 21:34:21 +0100

    path

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import {useSelector} from "react-redux";
 import FullDay from "src/copmonents/Days/FullDay/FullDay.jsx";
 import Month from "src/copmonents/Month/Month.jsx";
+import getDaysInMonth from "src/utils/daysInMonth.js";
 
 
 import Calendar from "./copmonents/Calendar/Calendar.jsx";
@@ -11,7 +12,69 @@ import App from './App.jsx'
 import './index.scss'
 
 import {createBrowserRouter, Link, RouterProvider} from "react-router-dom";
-
+const currentMonth = new Date().getMonth()
+const  months =  [
+	{
+		name: "January",
+		days: 31,
+		date: []
+	},
+	{
+		name: "February",
+		days: getDaysInMonth(1, 2024),
+		date: []// 29 в високосном году
+	},
+	{
+		name: "March",
+		days: 31,
+		date: []
+	},
+	{
+		name: "Aprel",
+		days: 30,
+		date: []
+	},
+	{
+		name: "May",
+		days: 31,
+		date: []
+	},
+	{
+		name: "June",
+		days: 30,
+		date: []
+	},
+	{
+		name: "July",
+		days: 31,
+		date: []
+	},
+	{
+		name: "August",
+		days: 31,
+		date: []
+	},
+	{
+		name: "September",
+		days: 30,
+		date: []
+	},
+	{
+		name: "October",
+		days: 31,
+		date: []
+	},
+	{
+		name: "November",
+		days: 30,
+		date: []
+	},
+	{
+		name: "December",
+		days: 31,
+		date: []
+	},
+]
 const router = createBrowserRouter([{
 	path: '/',
 	element: <App/>,
@@ -37,7 +100,7 @@ const router = createBrowserRouter([{
 			element: <FullDay/>
 		},
 	]
-}], {basename: '/calendar/february'})
+}], {basename: `calendar/${months[currentMonth].name.toLowerCase()}`})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>

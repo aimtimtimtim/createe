cdc1501131356a90f9a106dbf220609463067259
Tim <agayevdev@gmail.com>
2024-02-21 22:49:20 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.scss'
 import React from "react";
 import {Provider} from "react-redux";
 import {BrowserRouter, Link, Outlet, Routes, useLocation} from "react-router-dom";
-import Calendar from "./copmonents/Calendar/Calendar.jsx";
+
 import Sidebar from "./copmonents/Sidebar/Sidebar.jsx";
 import store from './store/store.js'
 
--- src/copmonents/Month/Month.jsx
+++ src/copmonents/Month/Month.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import {useOutletContext, useParams} from "react-router-dom";
+
 import styles from "./styles.module.scss";
 import Day from "src/copmonents/Days/Day/Day.jsx";
 
-export const Month = () => {
-	const [calendarDays, monthToChange, monthPath] = useOutletContext()
-
+export const Month = ({calendarDays, changeCurrentMonth, monthPath }) => {
 	return (
 		<div className={styles.days}>
 			{calendarDays.map((dayInCalendar, i) => (
 				dayInCalendar === null ? <p></p> :
 					<Day
-						key={dayInCalendar}
+						key={i}
 						monthPath={monthPath}
 						dayInCalendar={dayInCalendar}
-						selectedMonth={monthToChange}/>
+						changeCurrentMonth={changeCurrentMonth}/>
 			))}
 		
 		</div>
